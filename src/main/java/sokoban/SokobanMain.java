29c1e3072be1c5a5b5b39c95f0fe263a6102121c
Coemgen G <112963320+Calu12@users.noreply.github.com>
2024-10-16 02:40:24 +0200

    Update SokobanMain.java

--- src/main/java/sokoban/SokobanMain.java
+++ src/main/java/sokoban/SokobanMain.java
@@ -4,6 +4,12 @@ import com.codingame.gameengine.runner.SoloGameRunner;
 
 public class SokobanMain {
     public static void main(String[] args) {
+        String niveau = "test15.json";
+
+        jsonCharArray.main(new String[]{niveau});
+
+        MyPlanner.main(new String[]{"src\\main\\java\\sokoban\\domain.pddl", "src\\main\\java\\sokoban\\sokoban_problem.pddl"})
+
         SoloGameRunner gameRunner = new SoloGameRunner();
         gameRunner.setAgent(Agent.class);
         gameRunner.setTestCase("test11.json");
